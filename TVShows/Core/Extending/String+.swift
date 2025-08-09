bfc030c53510a622ea11e3f898f204f71ecc2920
Thomás Pereira <thomasccpereira@Thomas-Air.local>
2025-08-09 08:48:58 -0300

    created some helper extensions for networking

---
+++ TVShows/Core/Extending/String+.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension String {
+   var isValidURL: Bool {
+      let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+      if let match = detector?.firstMatch(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count)) {
+         return match.range.length == self.utf16.count
+      }
+      return false
+   }
+}
