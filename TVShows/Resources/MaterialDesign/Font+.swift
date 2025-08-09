878877d69d402f5ff1e4a6c5f4aaaf4b0decf829
Thomás Pereira <thomasccpereira@Thomas-Air.local>
2025-08-09 09:50:47 -0300

    created Font helper

---
+++ TVShows/Resources/MaterialDesign/Font+.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import Foundation
+
+extension Font {
+   // Thin
+   static let thin8: Font = .system(size: 8, weight: .thin)
+   static let thin10: Font = .system(size: 10, weight: .thin)
+   static let thin11: Font = .system(size: 11, weight: .thin)
+   static let thin12: Font = .system(size: 12, weight: .thin)
+   static let thin13: Font = .system(size: 13, weight: .thin)
+   static let thin14: Font = .system(size: 14, weight: .thin)
+   static let thin16: Font = .system(size: 16, weight: .thin)
+   static let thin18: Font = .system(size: 18, weight: .thin)
+   // Regular
+   static let regular8: Font = .system(size: 8)
+   static let regular10: Font = .system(size: 10)
+   static let regular11: Font = .system(size: 11)
+   static let regular12: Font = .system(size: 12)
+   static let regular13: Font = .system(size: 13)
+   static let regular14: Font = .system(size: 14)
+   static let regular16: Font = .system(size: 16)
+   static let regular18: Font = .system(size: 18)
+   // Medium
+   static let medium8: Font = .system(size: 8, weight: .medium)
+   static let medium10: Font = .system(size: 10, weight: .medium)
+   static let medium11: Font = .system(size: 11, weight: .medium)
+   static let medium12: Font = .system(size: 12, weight: .medium)
+   static let medium13: Font = .system(size: 13, weight: .medium)
+   static let medium14: Font = .system(size: 14, weight: .medium)
+   static let medium16: Font = .system(size: 16, weight: .medium)
+   static let medium18: Font = .system(size: 18, weight: .medium)
+   // Bold
+   static let bold8: Font = .system(size: 8, weight: .bold)
+   static let bold10: Font = .system(size: 10, weight: .bold)
+   static let bold11: Font = .system(size: 11, weight: .bold)
+   static let bold12: Font = .system(size: 12, weight: .bold)
+   static let bold13: Font = .system(size: 13, weight: .bold)
+   static let bold14: Font = .system(size: 14, weight: .bold)
+   static let bold16: Font = .system(size: 16, weight: .bold)
+   static let bold18: Font = .system(size: 18, weight: .bold)
+}
