57b912ad80089e66fced62598b105b141d514efb
Thomás Pereira <thomasccpereira@Thomas-Air.local>
2025-08-09 07:45:04 -0300

    created Xcode project

---
+++ TVShows/TVShowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVShowsApp.swift
+//  TVShows
+//
+//  Created by Thomás Pereira on 09/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TVShowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TVShowsTests/TVShowsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TVShowsTests.swift
+//  TVShowsTests
+//
+//  Created by Thomás Pereira on 09/08/25.
+//
+
+import Testing
+@testable import TVShows
+
+struct TVShowsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
